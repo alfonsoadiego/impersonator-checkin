e84b46567db46ef11a0d4ba441fbc276cfd47c00
alfonso.adiego <alfonso.adiego@gmail.com>
2020-01-27 18:25:27 +0100

    Initial commit

---
+++ src/main/java/org/acwar/impersonator/ImpersonatorApplication.java
@@ -0,0 +1,13 @@
+package org.acwar.impersonator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpersonatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpersonatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/acwar/impersonator/ImpersonatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acwar.impersonator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpersonatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
